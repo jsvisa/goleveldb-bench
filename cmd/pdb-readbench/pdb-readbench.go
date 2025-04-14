ad97f567479f5aeffa2574b110112cbdccb779f1
jsvisa <delweng@gmail.com>
2025-04-14 14:52:17 +0000

    pdb-read: add compaction metrics
    Signed-off-by: jsvisa <delweng@gmail.com>

--- cmd/pdb-readbench/pdb-readbench.go
+++ cmd/pdb-readbench/pdb-readbench.go
@@ -17,7 +17,44 @@ import (
 	"github.com/cockroachdb/pebble"
 	"github.com/cockroachdb/pebble/bloom"
 	bench "github.com/fjl/goleveldb-bench"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	compCount = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "compact_count",
+		Help: "The total number of compactions",
+	})
+	compReadCount = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "compact_read_count",
+		Help: "The total number of compaction reads",
+	})
+	compMoveCount = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "compact_move_count",
+		Help: "The total number of compaction moves",
+	})
+	compRewriteCount = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "compact_rewrite_count",
+		Help: "The total number of compaction rewrites",
+	})
+	compMultilevelCount = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "compact_multilevel_count",
+		Help: "The total number of compaction moves",
+	})
+	compEstimatedDebt = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "compact_estimated_debt",
+		Help: "The estimated debt of the compaction",
+	})
+	compMarkedFiles = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "compact_marked_files",
+		Help: "The number of marked files",
+	})
+	compDuration = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "compact_duration",
+		Help: "The total duration of compactions",
+	})
 )
 
 func main() {
@@ -313,8 +350,15 @@ func metric(db *pebble.DB, done chan struct{}) {
 		case <-done:
 			return
 		case <-timer.C:
-			metrics := db.Metrics()
-			log.Printf("Metrics: \n-------------------------------------------------------------------------------------------------------------------\n%+v\n", metrics)
+			stats := db.Metrics()
+			compCount.Set(float64(stats.Compact.Count))
+			compReadCount.Set(float64(stats.Compact.ReadCount))
+			compMoveCount.Set(float64(stats.Compact.MoveCount))
+			compRewriteCount.Set(float64(stats.Compact.RewriteCount))
+			compMultilevelCount.Set(float64(stats.Compact.MultiLevelCount))
+			compEstimatedDebt.Set(float64(stats.Compact.EstimatedDebt))
+			compMarkedFiles.Set(float64(stats.Compact.MarkedFiles))
+			compDuration.Set(float64(stats.Compact.Duration.Seconds()))
 		}
 	}
 }
